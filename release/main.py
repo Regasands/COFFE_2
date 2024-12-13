35b7353cb8daf25306412f94950cba385d25db88
Artemii <cvindergocvin@gmail.com>
2024-12-13 13:37:24 +0300

    upd

--- release/main.py
+++ release/main.py
@@ -75,15 +75,15 @@ class Coffe(QWidget, main_ui.Ui_Form):
             q.exec()
         else:
             q = Dialog({})
-            q.exec()
-            self.cur.execute('''SELECT * FROM Coffee''')
-            res = self.cur.fetchall()
-            self.tableWidget.setRowCount(0)
-            self.tableWidget.setRowCount(len(res)) 
-            for row_index, row_data in enumerate(res):
-                for col_index, col_data in enumerate(row_data):
-                    self.tableWidget.setItem(row_index, col_index, QTableWidgetItem(str(col_data)))
-            self.upd.clicked.connect(self.update_coffe)
+        q.exec()
+        self.cur.execute('''SELECT * FROM Coffee''')
+        res = self.cur.fetchall()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setRowCount(len(res)) 
+        for row_index, row_data in enumerate(res):
+            for col_index, col_data in enumerate(row_data):
+                self.tableWidget.setItem(row_index, col_index, QTableWidgetItem(str(col_data)))
+        self.upd.clicked.connect(self.update_coffe)
 
 
 
